8f535c8449ccd7775c8ed0341ade52fa05684ad3
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2024-12-26 23:16:33 +0530

    Connection to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to Database!")
+}).catch((err)=>{
+    console.log(err);
+});
 const app=express();
-
-app.listen(5173,()=>{
+app.listen(3000,()=>{
     console.log("Server running...");
 });
