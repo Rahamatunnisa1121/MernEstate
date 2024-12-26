787a4a7b544a70599f59b770027568dc305c04c6
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2024-12-26 19:57:17 +0530

    Created header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import About from './pages/About';
 import SignIn from './pages/Signin';
 import SignUp from './pages/Signup';
 import Profile from './pages/Profile';
-
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
