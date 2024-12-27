6336e5a6b497808e49b09da9722be2f79402fa7b
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2024-12-27 22:02:27 +0530

    Created header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-pink-500'>MERN</span>
+                    <span className='text-pink-600'>MERN</span>
                     <span className='text-pink-700'>Estate</span>
                 </h1>
             </Link>
