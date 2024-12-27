1ad1987427eaf4b2d8d212828b19dace820e7a64
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2024-12-27 22:00:07 +0530

    Created user model

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,27 +3,27 @@ import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-pink-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-500'>MERN</span>
-                    <span className='text-slate-700'>Estate</span>
+                    <span className='text-pink-500'>MERN</span>
+                    <span className='text-pink-700'>Estate</span>
                 </h1>
             </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <form className='bg-pink-100 p-3 rounded-lg flex items-center'>
                 <input type="text" placeholder="Search.." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <FaSearch className='text-slate-600' />
+                <FaSearch className='text-pink-600' />
             </form>
             <ul className='flex gap-4'>
                 <Link to='/'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                    <li className='hidden sm:inline text-pink-700 hover:underline font-medium'>Home</li>
                 </Link>
                 <Link to='/about'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+                    <li className='hidden sm:inline text-pink-700 hover:underline font-medium'>About</li>
                 </Link>
                 <Link to='/sign-in'>
-                    <li className='text-slate-700 hover:underline'>Sign in</li>
+                    <li className='text-pink-700 hover:underline font-medium'>Sign in</li>
                 </Link>
             </ul>
         </div>
