8c797fd779e984288560c8bdded86fce99c7499a
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2025-01-01 16:17:22 +0530

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link,useNavigate } from 'react-router-dom';
 import {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInFailure,signInStart,signInSuccess } from '../redux/user/userSlice.js';
 export default function SignIn() {
   const [formData,setFormData]=useState({});
-  const [error,setError]=useState(null);
-  const [loading,setLoading]=useState(false);
+  // const [error,setError]=useState(null);
+  // const [loading,setLoading]=useState(false);
+  const {loading,error}=useSelector((state)=>state.user);//to select the user slice from the store(global state)
   const navigate=useNavigate();
+  const dispatch=useDispatch();
   const handleChange=(e)=>{
     setFormData({
         ...formData,
@@ -16,7 +20,8 @@ export default function SignIn() {
     e.preventDefault(); //to prevent refreshing the page on submit
     try
     {
-      setLoading(true);
+      //setLoading(true);
+      dispatch(signInStart());
       const res=await fetch('/api/auth/signin',{
         method:'POST',
         headers:{
@@ -28,18 +33,21 @@ export default function SignIn() {
       console.log(data);
       if(data.success===false)
       {
-        setError(data.message);
-        setLoading(false);
+        // setError(data.message);
+        // setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-        setLoading(false);
-        setError(null);
+        // setLoading(false);
+        // setError(null);
+        dispatch(signInSuccess(data));
         navigate('/'); //hook
     }
     catch(error)
     {
-      setLoading(false);
-      setError(error.message);
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   //console.log(formData);
