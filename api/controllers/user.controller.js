15211e5148a3565c481d0c7f1d2d60a5c4c82a62
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2025-01-17 20:13:35 +0530

    completed user profile updation api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,35 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
 export const test = (req,res)=>{
     res.json({
         message:"Hello world!"
     });
+};
+export const updateUserInfo=async (req,res,next)=>{
+    if(req.user.id !== req.params.id)
+    {
+        return next(errorHandler(401,'You can only update your account, Donot try to update other accounts!'));
+    }
+    try
+    {
+        if(req.body.password)
+        {
+            req.body.password=bcryptjs.hashSync(req.body.password,10);
+        }
+        const updatedUser=await User.findByIdAndUpdate(req.params.id,{
+        $set:
+        {
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password,
+        }},
+        {new:true});
+        const {password,...rest}=updatedUser._doc;
+        res.status(200).json(rest);
+    }
+    catch(error)
+    {
+        next(error);
+    }  
 };
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import updateRouter from './routes/update.route.js';
+import cookieParser from 'cookie-parser';//initilaizing
 import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app=express();
 app.use('/uploads', express.static(path.resolve('uploads')));
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000,()=>{
     console.log("Server running...");
 });
