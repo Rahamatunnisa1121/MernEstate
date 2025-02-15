a65788a1a84896a4dc626d34a49c86c2ee053515
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2025-02-15 12:08:11 +0530

    comlete showUserListing Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,9 +8,13 @@ export default function Profile() {
   const fileRef=useRef(null);
   const [file,setFile]=useState(undefined);
   const [formData,setFormData]=useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError,setShowListingsError]=useState(false);
+  const [fetching,setFetching]=useState(false);
   //console.log(file);
   const {currentUser,loading,error}=useSelector((state)=>state.user);
   //console.log("Avatar URL:", currentUser?.avatar);
+  const [userListings,setUserListings]=useState([]);
   const handleFileUpload=async (file)=>{
     const formData=new FormData();
     formData.append('avatar',file);
@@ -66,13 +70,14 @@ export default function Profile() {
         body:JSON.stringify(formData)
       });
       const data=await res.json();
-      console.log(data);
+      //console.log(data);
       if(data.success===false)
       {
         dispatch(updateUserFailure(data.message));
         return;
       }
       dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
     }
     catch(err)
     {
@@ -120,6 +125,30 @@ export default function Profile() {
       //console.log(err);
     }
   };
+  const handleShowListings=async()=>{
+    try
+    {
+      setFetching(true);
+      setShowListingsError(false);
+      const res=await fetch(`/api/user/listings/${currentUser._id}`);
+      const data= await res.json();
+      if(data.success==false)
+      {
+        setFetching(false);
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+      //console.log(data);
+      setFetching(false);
+    }
+    catch(error)
+    {
+      setFetching(false);
+      setShowListingsError(true);
+      //console.log(error);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7 text-pink-700'>Profile</h1>
@@ -148,6 +177,28 @@ export default function Profile() {
         <span onClick={handleSignOut} className='text-pink-800 cursor-pointer'>Sign out</span>
       </div>
       {error && <p className='text-red-600 mt-5 font-bold'>{error}</p>}
+      {updateSuccess &&<p className='text-green-600 mt-5 font-bold'>User updated successfully!</p>}
+      <button onClick={handleShowListings} disabled={fetching} className='text-pink-800 w-full font-semibold'>{fetching?'Fetching..':'Show Listings'}</button>
+      <p className='text-red-600 mt-5 font-bold'>{showListingsError?'Error showing listings..':''}</p>
+      {userListings && userListings.length>0 &&
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-xl text-cyan-700 font-semibold'>Your Listings</h1>
+          {userListings.map((listing)=>
+          <div key={listing._id} className='border border-pink-300 rounded-lg p-3 flex justify-between items-center gap-4'>
+            <Link to={`/listing/${listing._id}`}>
+              <img src={listing.imageUrls[0]} alt="Listing cover" className='h-16 w-16 object-contain'/>
+            </Link>
+            <Link className='flex-1 text-black-500 font-semibold hover:underline truncate' to={`/listing/${listing._id}`}>
+              <p>{listing.name}</p>
+            </Link>
+            <div className='flex flex-col items-center'>
+              <button className='text-red-600 uppercase font-semibold'>Delete</button>
+              <button className='text-green-600 uppercase font-semibold'>Edit</button>
+              </div>
+          </div>
+        )}
+        </div>
+      }
     </div>
   )
 }
